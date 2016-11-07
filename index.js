87da524e82b22c967569a53aef6ba3bc0ee1a493
Jim Kang <jimkang@gmail.com>
2016-11-07 09:43:33 -0500

    Extracted from godtributes, support for non-nouns mode.

---
+++ index.js
@@ -0,0 +1,168 @@
+var request = require('request');
+var sb = require('standard-bail')();
+var pluck = require('lodash.pluck');
+var compact = require('lodash.compact');
+var queue = require('d3-queue').queue;
+var IsVerb = require('./is-verb');
+var callNextTick = require('call-next-tick');
+
+var underscoreRegex = /_/g;
+var uppercaseRegex = /[A-Z]/g;
+
+var badPhraseStarts = [
+  'TO_',
+  'WHEN_',
+  'WITH_',
+  'IN_PART',
+  'WILL_',
+  'SIEG_HEIL',
+  'CHING_CHING',
+  'FATTER_WALLET'
+];
+
+var badPhraseEnds = [
+  '_OF',
+  '_TO',
+  '_THE'
+];
+
+function GetWord2VecNeighbors({
+  gnewsWord2VecURL, nounfinder, probable, wordnok, nounLikePhrasesOnly, nounWordsOnly
+}) {
+
+  var isVerb = IsVerb({
+    wordnok: wordnok
+  });
+
+  return getWord2VecNeighbors;
+
+  function getWord2VecNeighbors(words, done) {
+    var wordsToGetNeighborsOf = probable.sample(
+      words, probable.rollDie(words.length)
+    );
+
+    var opts = {
+      method: 'GET',
+      json: true,
+      url: gnewsWord2VecURL,
+      qs: {
+        words: wordsToGetNeighborsOf.join(',')
+      }
+    };
+    request(opts, sb(pickWords, done));
+  }
+
+  function pickWords(res, body, done) {
+    if (body && body.message === 'Key not found in database') {
+      done();
+      return;
+    }
+    // console.log(JSON.stringify(body, null, '  '));
+    // In the context of w2v results, we can throw out two-letter words.
+    var words = pluck(body, 'word').filter(wordIsOK);
+    var phrases = [];
+    var normalWords = [];
+
+    words.forEach(putWordInBucket);
+debugger;
+    phrases = phrases.filter(phraseIsOK);
+
+    if (nounWordsOnly) {
+      var nounFindingQueue = queue(2);
+      nounFindingQueue.defer(nounfinder.getNounsFromWords, normalWords);
+      // WARNING: nounLikePhrasesOnly only kicks in if nounWordsOnly is true.
+      if (nounLikePhrasesOnly) {
+        nounFindingQueue.defer(getPhrasesEndingInNouns, phrases);
+      }
+      nounFindingQueue.awaitAll(recombineBuckets);
+    }
+    else {
+      callNextTick(done, null, normalWords.concat(phrases).map(replaceUnderscores));
+    }
+
+    function putWordInBucket(word) {
+      if (word.indexOf('_') === -1) {
+        normalWords.push(word);
+      }
+      else {
+        phrases.push(word);
+      }
+    }
+
+    function recombineBuckets(error, wordGroups) {
+      if (error) {
+        done(error);
+      }
+      else {
+        var wordNouns = wordGroups[0];
+        if (wordGroups.length > 1) {
+          var phrasesWithNouns = wordGroups[1];
+        }
+        // console.log('normalWords', normalWords, 'phrases', phrases);
+        // console.log('wordNouns', wordNouns, 'phrasesWithNouns', phrasesWithNouns);
+        done(
+          null, wordNouns.concat(compact(phrasesWithNouns).map(replaceUnderscores))
+        );
+      }
+    }
+
+    function getPhrasesEndingInNouns(phrases, done) {
+      var q = queue();
+      phrases.forEach(queueCheck);
+      q.awaitAll(done);
+
+      function queueCheck(phrase) {
+        q.defer(findOutIfPhraseIsSuitable, phrase);
+      }
+
+      function findOutIfPhraseIsSuitable(phrase, findDone) {
+        var phraseWords = phrase.split('_');
+        if (phraseWords.length < 1) {
+          callNextTick(findDone);
+        }
+        else {
+          var q = queue();
+          q.defer(nounfinder.getNounsFromWords, phraseWords.slice(-1));
+          q.defer(isVerb, phraseWords[0]);
+          q.await(passPhrase);
+        }
+
+        function passPhrase(error, phraseNouns, isVerb) {
+          if (error) {
+            findDone(error);
+          }
+          else {
+            findDone(null, phraseNouns.length > 0 && !isVerb ? phrase : null);
+          }
+        }        
+      }
+    }
+  }
+}
+
+function longerThanTwoChars(w) {
+  return w && w.length > 2;
+}
+
+function replaceUnderscores(w) {
+  return w.replace(underscoreRegex, ' ');
+}
+
+function wordIsOK(word) {
+  return word && word.length > 2 && !word.match(uppercaseRegex);
+}
+
+function phraseIsOK(phrase) {
+  return !badPhraseStarts.some(startsWith) &&
+    !badPhraseEnds.some(endsWith);
+
+  function startsWith(badStart) {
+    return phrase.startsWith(badStart);
+  }
+
+  function endsWith(badEnd) {
+    return phrase.endsWith(badEnd);
+  }
+}
+
+module.exports = GetWord2VecNeighbors;
---
+++ tests/basictests.js
@@ -0,0 +1,130 @@
+var test = require('tape');
+var GetWord2VecNeighbors = require('../index');
+var assertNoError = require('assert-no-error');
+var createNounfinder = require('nounfinder');
+var createProbable = require('probable').createProbable;
+var seedrandom = require('seedrandom');
+var createWordnok = require('wordnok').createWordnok;
+var config = require('../config');
+
+var cases = [
+  {
+    name: 'Words from vision',
+    seed: 'vision',
+    words: ['logo', 'brand'],
+    expected: ['trademark infringement'],
+    nounWordsOnly: true
+  },
+  {
+    name: 'empathy, trump',
+    seed: 'empathy trump',
+    words: ['empathy', 'trump'],
+    expected: ['desperateness'],
+    nounWordsOnly: true
+  },
+  {
+    name: 'Weird short words',
+    seed: 'weird',
+    words: ['comp', 'ar'],
+    expected: [ 'crit', 'uni'],
+    nounWordsOnly: true
+  },
+  {
+    name: 'Vermin',
+    seed: 'vermin',
+    words: ['mice', 'spider', 'insect'],
+    expected:[ 'critter', 'snake', 'grasshopper', 'squirrel', 'critter', 'bee', 'gopher', 'insect eater', 'creepy critters', 'woolly bears' ],
+    nounWordsOnly: true
+  },
+  {
+    name: 'Not in word2vec model',
+    seed: 'missing',
+    words: ['match-up'],
+    expected: undefined,
+    nounWordsOnly: true
+  },
+  {
+    name: 'Filter dumb phrases',
+    seed: 'filter',
+    words: ['promenade', 'spittal'],
+    expected: [ 'promenade', 'seafront', 'quay', 'piazza', 'quayside', 'quay', 'seafront promenade', 'roof terrace', 'pavement cafés', 'cobblestone lanes' ],
+    nounWordsOnly: true
+  },
+
+  {
+    name: 'Words from vision (non-nouns allowed)',
+    seed: 'vision',
+    words: ['logo', 'brand'],
+    expected: ['trademark infringement'],
+    nounWordsOnly: false
+  },
+  {
+    name: 'empathy, trump (non-nouns allowed)',
+    seed: 'empathy trump',
+    words: ['empathy', 'trump'],
+    expected: [ 'genuine', 'underrates', 'indicitive', 'verbalized', 'true', 'desperateness', 'woefully misplaced', 'elicit yawns', 'elicits giggles', 'defy logic', 'elicit groans', 'conveniently glossed' ],
+    nounWordsOnly: false
+  },
+  {
+    name: 'Weird short words (non-nouns allowed)',
+    seed: 'weird',
+    words: ['comp', 'ar'],
+    expected: [ 'crit', 'obv', 'uni', 'grat', 'actaully', 'acctually' ],
+    nounWordsOnly: false
+  },
+  {
+    name: 'Vermin (non-nouns allowed)',
+    seed: 'vermin',
+    words: ['mice', 'spider', 'insect'],
+    expected: [ 'critter', 'snake', 'grasshopper', 'squirrel', 'critters', 'bee', 'gopher', 'insect eater', 'creepy crawly', 'creepy critters', 'woolly bears' ],
+    nounWordsOnly: false
+  },
+  {
+    name: 'Not in word2vec model (non-nouns allowed)',
+    seed: 'missing',
+    words: ['match-up'],
+    expected: undefined,
+    nounWordsOnly: false
+  },
+  {
+    name: 'Filter dumb phrases (non-nouns allowed)',
+    seed: 'filter',
+    words: ['promenade', 'spittal'],
+    expected: [ 'promenades', 'seafront', 'quay', 'piazza', 'quayside', 'quays', 'seafront promenade', 'roof terrace', 'pavement cafés', 'cobblestone lanes' ],
+    nounWordsOnly: false
+  }
+];
+
+var nounfinder = createNounfinder({
+  wordnikAPIKey: config.wordnikAPIKey
+});
+
+var wordnok =  createWordnok({
+  apiKey: config.wordnikAPIKey
+});
+
+cases.forEach(runTest);
+
+function runTest(testCase) {
+  test(testCase.name, runTest);
+
+  function runTest(t) {
+    var getWord2VecNeighbors = GetWord2VecNeighbors({
+      gnewsWord2VecURL: config.gnewsWord2VecURL,
+      nounfinder: nounfinder,
+      probable: createProbable({
+        random: seedrandom(testCase.seed)
+      }),
+      wordnok: wordnok,
+      nounLikePhrasesOnly: true,
+      nounWordsOnly: testCase.nounWordsOnly
+    });
+    getWord2VecNeighbors(testCase.words, checkNeighbors);
+
+    function checkNeighbors(error, neighbors) {
+      assertNoError(t.ok, error, 'No error while getting neighbors.');
+      t.deepEqual(neighbors, testCase.expected, 'Neighbors are correct.');
+      t.end();
+    }
+  }
+}
